79eb7f34ce0687b1211668fb281faf0a2aa9a11f
Juan Fernandes <juan.fernandes@eits.com.br>
2018-08-03 15:48:06 -0300

    clean project

---
+++ mobile-solution/xamarinrest/xamarinrest/Services/Rest/RestHolder.cs
@@ -0,0 +1,85 @@
+﻿using xamarinrest.Configuration;
+using xamarinrest.Database;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarinrest.Services.Rest
+{
+    class RestHolder<T>
+    {
+        private static readonly TimeSpan refreshTimeSpan = new TimeSpan( 0, 0, 15 );
+
+        //self instance singleton para RestEntityHolder, que auxilia a mexer com as Uri's REST
+        public static RestHolder<T> instance = null;
+
+        private string _syncUri;
+        private string _insertUri;
+        private string _updateUri;
+        private string _deleteUri;
+
+        public RestHolder()
+        {
+            instance = instance ?? this;
+        }
+
+        public string InsertUri
+        {
+            get => _insertUri ?? typeof(T).Name.ToLower() + "/" + "insert";
+            set => _insertUri = value;
+        }
+
+        public string UpdateUri
+        {
+            get => _updateUri ?? typeof(T).Name.ToLower() + "/" + "update";
+            set => _updateUri = value;
+        }
+
+        public string DeleteUri
+        {
+            get => _deleteUri ?? typeof(T).Name.ToLower() + "/" + "delete";
+            set => _deleteUri = value;
+        }
+
+        public string SyncUri
+        {
+            get => _syncUri ?? typeof(T).Name.ToLower() + "/" + "list";
+            set => _syncUri = value;
+        }
+
+        public static void StartAutoSync<T>() where T : new()
+        {
+            RunTask<T>();
+
+            Device.StartTimer( refreshTimeSpan, () => {
+                RunTask<T>();
+                return true; //restart timer
+            });
+        }
+
+        private static async void RunTask<T>() where T : new()
+        {
+            try
+            {
+                //Pega o DateTime da ultima requisição desta Uri
+                DateTime lastRequest = Prefs.getDateTime(RestHolder<T>.instance.SyncUri);
+
+                //Request e Sync
+                long unixTimestamp = lastRequest.Ticks - new DateTime(1970, 1, 1).Ticks;
+                string content2 = await RestService.GetAsync(RestHolder<T>.instance.SyncUri + ( unixTimestamp / TimeSpan.TicksPerMillisecond ) );
+                List<T> list2 = JsonConvert.DeserializeObject<List<T>>(content2);
+                SQLiteRepository.Sync<T>(list2);
+
+                //Seta o DateTime da ultima requisição para AGORA
+                Prefs.setDateTime(RestHolder<T>.instance.SyncUri, DateTime.Now);
+            }
+            catch( Exception e )
+            {
+                Console.WriteLine( e.Message );
+            }
+        }
+    }
+}
---
+++ mobile-solution/xamarinrest/xamarinrest/Services/Rest/RestService.cs
@@ -0,0 +1,65 @@
+﻿using xamarinrest.Models;
+using xamarinrest.Services.Rest;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xamarinrest.Services
+{
+    class RestService
+    {
+        private static readonly string Url = "http://192.168.20.195:8080/";
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static void Init()
+        {
+            //Obrigatório; Cria a configuração de REST para a entidade T especificada em <T>
+            var pessoaHolder = new RestHolder<Pessoa> {
+                SyncUri = "pessoa/merge?date=",
+                InsertUri = "pessoa/insert",
+                UpdateUri = "pessoa/update",
+                DeleteUri = "pessoa/delete/" //concat ID
+            };
+
+            //...
+
+            //Cria um timer que executa uma chamada para "SyncUri" automáticamente a cada X periodos
+            RestHolder<Pessoa>.StartAutoSync<Pessoa>();
+            
+            //...
+        }
+
+        //generic method to send entity with rest HTTP PUT/POST
+        public static async void Send<T>( string fullUri, Object entity ) where T : new()
+        {
+            var holder = RestHolder<T>.instance;
+            string content = JsonConvert.SerializeObject( entity );
+            StringContent restContent = new StringContent(content, Encoding.UTF8, "application/json");
+            await _client.PostAsync( fullUri, restContent );
+        }
+
+        //generic method to insert/update with rest HTTP PUT/POST
+        public static void SendEntity<T>( AbstractEntity entity ) where T : new()
+        {
+            var holder = RestHolder<T>.instance;
+            if ( entity.Id == null ) Send<T>( Url + holder.InsertUri, entity );
+            else Send<T>( Url + holder.UpdateUri, entity );
+        }
+
+        //generic method to delete with rest HTTP DELETE
+        public static async void Delete<T>( long id ) where T : new()
+        {
+            var holder = RestHolder<T>.instance;
+            await _client.DeleteAsync( Url + holder.DeleteUri + id );
+        }
+
+        //generic method to rest HTTP GET
+        public static async Task<string> GetAsync( string uri )
+        {
+            string content = await _client.GetStringAsync(Url + uri);
+            return await Task.FromResult( content );
+        }
+    }
+}
